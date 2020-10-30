e3bb4ecf3d4eda8fe6f14836f7225654049855c1
Tyler Linen <66691586+tylerlinen@users.noreply.github.com>
2020-10-30 16:21:40 -0400

    updated

--- models/workout.js
+++ models/workout.js
@@ -46,7 +46,11 @@ const WorkoutSchema = new Schema(
     }}
 
 )
-
+WorkoutSchema.virtual("totalDuration").get(function(){
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+      }, 0);
+})
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,58 @@ const router = require("express").Router();
 const Workout = require("../models/workout")
 
 
-router.get("/api/exercise", (req, res) => {
- Workout.find({}).then(dbWorkout => res.json(dbWorkout))
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
 })
 
+router.post("/api/workouts/range", (req, res) => {
+    Workout.create({})
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+})
 
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { exercises: body } },
+        { new: true, runValidators: true }
+    )
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+})
 
 
 module.exports = router;
